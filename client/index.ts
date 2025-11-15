def63310dc3480d15ef4cc7e415ed7d83b250e06
shangxueink <1919892171@qq.com>
2025-11-15 22:59:19 +0800

    格式化代码

--- client/index.ts
+++ client/index.ts
@@ -6,20 +6,20 @@ import './icons';
 
 export default (ctx: Context) =>
 {
-    ctx.page({
-        name: '云湖官网控制台',
-        path: '/yunhu-control',
-        desc: "",
-        authority: 4,
-        icon: 'activity:yunhu',
-        component: defineComponent({
-            setup()
-            {
-                const iframe = ref<HTMLIFrameElement>();
-                return () => h(resolveComponent('k-layout'), {}, {
-                    default: () => h('iframe', { ref: iframe, src: "https://www.yhchat.com/control", class: 'layout-iframe' }),
-                });
-            },
-        }),
-    });
+  ctx.page({
+    name: '云湖官网控制台',
+    path: '/yunhu-control',
+    desc: "",
+    authority: 4,
+    icon: 'activity:yunhu',
+    component: defineComponent({
+      setup()
+      {
+        const iframe = ref<HTMLIFrameElement>();
+        return () => h(resolveComponent('k-layout'), {}, {
+          default: () => h('iframe', { ref: iframe, src: "https://www.yhchat.com/control", class: 'layout-iframe' }),
+        });
+      },
+    }),
+  });
 };
